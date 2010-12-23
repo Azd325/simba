170eb40e8ea7fe6dbd597e2a0492f8f219ea4616
Tim Kleinschmidt <tim.kleinschmidt@googlemail.com>
2010-12-23 13:12:19 +0100

    add database methods open & delete

--- src/database.h
+++ src/database.h
@@ -2,11 +2,21 @@
 #define DATABASE_H
 
 #include <QObject>
+#include <QSqlDatabase>
+#include <QSqlError>
+#include <QFile>
+#include <QDir>
 
 class Database : public QObject
 {
 public:
-    Database();
+    public:
+        bool openDB();
+        bool deleteDB();
+        QSqlError lastError();
+ 
+    private:
+        QSqlDatabase db;
 };
 
 #endif // DATABASE_H
