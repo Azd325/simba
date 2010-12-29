d0975c0ea98aeaaf0009a59b1a75f537a9653fc5
Tim Kleinschmidt <tim.kleinschmidt@googlemail.com>
2010-12-29 23:53:44 +0100

    coding style

--- src/iconloader.cpp
+++ src/iconloader.cpp
@@ -25,24 +25,24 @@ QIcon IconLoader::Load( const QString &name ) {
 
 #if QT_VERSION >= 0x040600
   // Try to load it from the theme initially
-  ret = QIcon::fromTheme( name );
-  if ( !ret.isNull()) {
+  ret = QIcon::fromTheme ( name );
+  if ( !ret.isNull ()) {
     return ret;
   }
 #endif
 
   // Otherwise use our fallback theme
-  const QString path( ":/icons/%1x%2/%3.png" );
-  foreach (int size, sizes_) {
-    QString filename (path.arg( size ).arg( size ).arg( name ));
+  const QString path ( ":/icons/%1x%2/%3.png" );
+  foreach ( int size, sizes_ ) {
+    QString filename (path. arg( size ). arg( size ).arg ( name ));
 
     if ( QFile::exists ( filename )) {
       ret.addFile ( filename, QSize( size, size ));
     }
   }
 
-  if ( ret.isNull()) {
-    qWarning() << "Couldn't load icon" << name;
+  if ( ret.isNull ()) {
+    qWarning () << "Couldn't load icon" << name;
   }
   return ret;
 }
