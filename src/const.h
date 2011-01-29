d3b11330002545707387f703270731e383ae20e3
Tim Kleinschmidt <tim.kleinschmidt@googlemail.com>
2011-01-29 19:02:11 +0100

    add first constants

--- src/const.h
+++ src/const.h
@@ -1,4 +1,10 @@
 #ifndef CONST_H
 #define CONST_H
 
+#include <QString>
+
+const QString APP_NAME = "Simba";
+const QString APP_VERSION = "0.95";
+const QString APP_URL = "azd325.github.com/simba/";
+
 #endif // CONST_H
